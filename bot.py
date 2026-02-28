3bff66974d5c0ae975435ac7d1f7e8356ce5183e
p16014633-stack <p16014633@gmail.com>
2026-02-28 05:21:28 +0300

    Add bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+BOT_TOKEN = os.environ["8631864797:AAFwlHOZRAo1uYmuS77PW0rJel3oiYz2aqs
+"]
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🚀 البوت يعمل بنجاح!")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
